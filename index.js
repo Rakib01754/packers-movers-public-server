a2d5bafa962a8cd400f6b207cfcd061508ad3fb0
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-11-10 11:26:04 +0600

    vercel config added

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
         // send jwt from client side 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
             res.send({ token })
         })
 
